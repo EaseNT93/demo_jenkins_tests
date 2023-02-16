1c59dd020a158262f2e8fdcc157ac4cbf7da4a07
EaseNT93 <easent93@gmail.com>
2023-02-16 14:08:21 +0500

    add test

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+@pytest.fixture
+def init_test():
+
+    URL = 'https://www.google.com/'
+
+    return URL
---
+++ tests/test_google.py
@@ -0,0 +1,10 @@
+import requests
+
+
+class TestGoogleSearch():
+
+    def test_page_access(self, init_test):
+
+        response = requests.get(init_test())
+
+        assert response.status_code == 200
