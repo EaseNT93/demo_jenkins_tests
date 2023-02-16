72a1b7cd1f3a3c3d03a0aacd4d5e5348e4a9e835
EaseNT93 <easent93@gmail.com>
2023-02-16 14:13:56 +0500

    upd test

--- tests/test_google.py
+++ tests/test_google.py
@@ -5,6 +5,6 @@ class TestGoogleSearch():
 
     def test_page_access(self, init_test):
 
-        response = requests.get(init_test())
+        response = requests.get(init_test)
 
         assert response.status_code == 200
