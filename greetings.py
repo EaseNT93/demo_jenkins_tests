097acbecbb41b5b2f3476737bd989cd889bb5b3d
Artem <77778300+EaseNT93@users.noreply.github.com>
2023-02-21 16:09:51 +0500

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -1,6 +1,11 @@
 import sys
+from datetime import datetime
 
 jenkins_param = str(sys.argv[1])
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
 gret_msg = f'Hello {jenkins_param}, good luck!'
 
 print(gret_msg)
+print("Current Time =", current_time)
+
